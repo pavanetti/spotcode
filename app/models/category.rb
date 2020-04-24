f16fb69f567a39621ccead1bf0a189918a2d6f6f
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-24 13:32:35 -0300

    Update models with relactions and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class Category < ApplicationRecord
+  has_many :albums
+
+  validates :name, presence: true
+  has_one_attached :image
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Song < ApplicationRecord
   belongs_to :album
+
+  validates :title, presence: true
+  has_one_attached :file
 end
