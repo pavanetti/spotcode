375521bcfe91dfafe7d77657d94749b17115cd18
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-24 23:24:40 -0300

    Add albums controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :albums, only: :show
       resources :dashboard, only: :index
       resources :categories, only: %i[index show]
       resources :search, only: :index
