6b6341bf39cc6b40d6c62bb72de4256f90d429fb
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-24 22:21:49 -0300

    Add json response

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -3,6 +3,7 @@
 class Api::V1::DashboardController < ApplicationController
   def index
     load_recent_heard
+    load_recommendations
   end
 
   private
@@ -16,9 +17,9 @@ class Api::V1::DashboardController < ApplicationController
 
   def load_recommendations
     heard_categories = @recent_albums.map(&:category)
-    @recommend_albuns =
+    @recommend_albums =
       if heard_categories.present?
-        Album.joins(:category, :song)
+        Album.joins(:category, :songs)
              .where(category: heard_categories)
              .order('songs.played_count')
              .select('distinct albums.*')
