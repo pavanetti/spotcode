6198ed38b3d22dbf90f36ea2838130a24c432a9a
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-25 20:30:27 -0300

    Improve home screen

---
+++ app/javascript/components/common/section-wrapper/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Section } from "react-bulma-components";
+
+const SectionWrapper = ({ children }) => {
+  return (
+    <Section>
+      <Container>{children}</Container>
+    </Section>
+  );
+};
+
+export default SectionWrapper;
--- app/javascript/screens/home/index.js
+++ app/javascript/screens/home/index.js
@@ -1,10 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+import { Button, Columns, Heading } from "react-bulma-components";
+import SectionWrapper from "components/common/section-wrapper";
+
+const MainHeading = styled(Heading)`
+  margin-top: 50px;
+`;
+
+const DescriptionList = styled.ul`
+  margin-top: 50px;
+  font-size: 20px;
+  list-style-type: square;
+`;
+
+const ButtonSubscribe = styled(Button)`
+  border-width: 2px;
+  margin-top: 50px;
+`;
 
 const HomeScreen = () => {
   return (
-    <>
-      <div>HomeScreen</div>
-    </>
+    <SectionWrapper>
+      <Columns>
+        <Columns.Column>
+          <MainHeading className="has-text-weight-light has-text-centered has-text-white">
+            Seu app de <br />
+            <span className="has-text-success">música</span>
+          </MainHeading>
+        </Columns.Column>
+      </Columns>
+      <Columns className="is-centered is-mobile">
+        <Columns.Column
+          mobile={{ size: 8, offset: 1 }}
+          desktop={{ size: 4, offset: 2 }}
+        >
+          <DescriptionList className="has-text-white">
+            <li>
+              Suas músicas <b>favoritas</b>
+            </li>
+            <li>
+              Seus <b>podcasts</b>
+            </li>
+            <li>
+              As novas <b>descobertas</b>
+            </li>
+          </DescriptionList>
+        </Columns.Column>
+      </Columns>
+      <Columns className="has-text-centered">
+        <Columns.Column>
+          <a href="/users/sign_up">
+            <ButtonSubscribe className="is-success is-outlined is-large has-text-white">
+              Cadastrar agora
+            </ButtonSubscribe>
+          </a>
+        </Columns.Column>
+      </Columns>
+    </SectionWrapper>
   );
 };
 
