489864fbbbc12ea9c520bb12494cdf2de80afc32
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-25 20:11:45 -0300

    Update menu to handle sign-in state

--- app/javascript/components/common/menu/index.js
+++ app/javascript/components/common/menu/index.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import {
-  Navbar,
-  Container,
-  Columns,
-  Image,
   Button,
+  Columns,
+  Container,
+  Dropdown,
+  Image,
+  Navbar,
 } from "react-bulma-components";
 import styled from "styled-components";
 
+import { FaUserCircle } from "react-icons/fa";
 import logo from "assets/images/logo.png";
 
 const ColumnsFullWidth = styled(Columns)`
@@ -16,25 +19,46 @@ const ColumnsFullWidth = styled(Columns)`
   padding-left: 10px;
   padding-bottom: 10px;
 `;
-const Menu = () => (
-  <>
-    <Navbar color="dark">
-      <Container>
-        <ColumnsFullWidth className="is-mobile">
-          <Columns.Column desktop={{ size: 2 }} mobile={{ size: 5 }}>
-            <Image src={logo} />
-          </Columns.Column>
-          <Columns.Column>
-            <a href="/users/sign_in" className="is-pulled-right is-right">
-              <Button outlined={true} color="white">
-                Entrar
-              </Button>
-            </a>
-          </Columns.Column>
-        </ColumnsFullWidth>
-      </Container>
-    </Navbar>
-  </>
-);
+
+const Menu = () => {
+  const location = useLocation();
+
+  const actionButton =
+    location.pathname == "/" ? (
+      <a href="/users/sign_in" className="is-pulled-right is-right">
+        <Button outlined={true} color="white">
+          Entrar
+        </Button>
+      </a>
+    ) : (
+      <Dropdown
+        className="is-pulled-right is-right"
+        color="dark"
+        label={<FaUserCircle size="2em" />}
+      >
+        <Dropdown.Item value="other">
+          <a href="/users/edit">Edit User</a>
+        </Dropdown.Item>
+        <Dropdown.Item value="other">
+          <a href="/users/sign_out">Log Out</a>
+        </Dropdown.Item>
+      </Dropdown>
+    );
+
+  return (
+    <>
+      <Navbar color="dark">
+        <Container>
+          <ColumnsFullWidth className="is-mobile">
+            <Columns.Column desktop={{ size: 2 }} mobile={{ size: 5 }}>
+              <Image src={logo} />
+            </Columns.Column>
+            <Columns.Column>{actionButton}</Columns.Column>
+          </ColumnsFullWidth>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default Menu;
