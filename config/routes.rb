66879f47ad2f69ec1fc5a86d397a870e5b0a9ade
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-24 23:01:23 -0300

    Add search controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :dashboard, only: :index
       resources :categories, only: :index
+      resources :search, only: :index
     end
   end
 end
