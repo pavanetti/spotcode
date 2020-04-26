cb8b68ded0ef07d1cf3757b96c858824a44f0ac8
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-25 22:54:36 -0300

    Update discovery to fetch api data

--- app/javascript/components/discovery/index.js
+++ app/javascript/components/discovery/index.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Columns, Heading } from "react-bulma-components";
+import AlbumsService from "services/albums";
 import Album from "../album";
 
 const DivVSpaced = styled.div`
@@ -8,31 +9,20 @@ const DivVSpaced = styled.div`
 `;
 
 const Discovery = () => {
-  const mock = [
-    {
-      id: 1,
-      title: "Gubernator",
-      artist_name: "Andrew Howes",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBDdz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--9c978ea9255b38d1230bcfec1dabd9bb9dd75583/gubernator.jpg",
-    },
-    {
-      id: 2,
-      title: "The Great Bear",
-      artist_name: "Andrew Howes",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBFQT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--abdbdc294430f45b852068658a385e72ae330212/the_great_bear.jpg",
-    },
-    {
-      id: 3,
-      title: "Barcelona",
-      artist_name: "Yellow Chair",
-      cover_url:
-        "/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBGUT09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--96c812663f514b2e7c70ccbb89ba3429573df5d6/barcelona.jpg",
-    },
-  ];
+  const [recentAlbums, setRecentAlbums] = useState([]);
+  const [recommendAlbums, setRecommendAlbums] = useState([]);
 
-  const albums = mock.map((album, key) => (
+  async function fetchAlbums() {
+    const response = await AlbumsService.index();
+    setRecentAlbums(response.data.recent_albums);
+    setRecommendAlbums(response.data.recommend_albums);
+  }
+
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
+  const columnFromAlbum = (album, key) => (
     <Columns.Column key={key} desktop={{ size: 3 }} mobile={{ size: 6 }}>
       <Album
         id={album.id}
@@ -41,22 +31,30 @@ const Discovery = () => {
         cover_url={album.cover_url}
       />
     </Columns.Column>
-  ));
+  );
 
   return (
     <>
-      <div>
-        <Heading className="has-text-white" size={4}>
-          Tocadas recentemente
-        </Heading>
-        <Columns className="is-mobile">{albums}</Columns>
-      </div>
-      <DivVSpaced>
-        <Heading className="has-text-white" size={4}>
-          Recomendadas
-        </Heading>
-        <Columns className="is-mobile">{albums}</Columns>
-      </DivVSpaced>
+      {recentAlbums.length > 0 && (
+        <div>
+          <Heading className="has-text-white" size={4}>
+            Tocadas recentemente
+          </Heading>
+          <Columns className="is-mobile">
+            {recentAlbums.map(columnFromAlbum)}
+          </Columns>
+        </div>
+      )}
+      {recommendAlbums.length > 0 && (
+        <DivVSpaced>
+          <Heading className="has-text-white" size={4}>
+            Recomendadas
+          </Heading>
+          <Columns className="is-mobile">
+            {recommendAlbums.map(columnFromAlbum)}
+          </Columns>
+        </DivVSpaced>
+      )}
     </>
   );
 };
